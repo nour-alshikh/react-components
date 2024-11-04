e18dccced24d0ef6eb3bbc655251a70cec124e89
nour-alshikh <nooralshikh30@gmail.com>
2024-11-04 21:09:03 +0200

    accordion component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Accordion from "./Components/Accordion"
+
+function App() {
+
+  return (
+    <>
+      <Accordion />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Accordion/Index.jsx
@@ -0,0 +1,65 @@
+import {data} from "./data.js";
+import {useState} from "react";
+
+const Index = () => {
+
+    const [selected, setSelected] = useState(null)
+    const [enableMultiSelect, setEnableMultiSelect] = useState(false)
+    const [multipleSelected, setMultipleSelected] = useState([])
+
+    const handleSingleSelect = (id)=>{
+        selected === id ? setSelected(null) : setSelected(id)
+    }
+
+    const handleMultiSelect = (id)=> {
+     let multiSelectedCpy = [...multipleSelected];
+        multiSelectedCpy.indexOf(id) === -1 ? multiSelectedCpy.push(id) : multiSelectedCpy.splice(multiSelectedCpy.indexOf(id) , 1);
+
+        setMultipleSelected(multiSelectedCpy);
+    }
+
+
+   const handleEnableMultiSelect = ()=>{
+       setEnableMultiSelect(!enableMultiSelect)
+       setSelected(null);
+       setMultipleSelected([])
+   }
+
+    return (
+        <>
+            <section className="" >
+                <div className="w-2/3 mx-auto my-2">
+                    <div className="text-center">
+                        <button type="button" className="rounded bg-blue-800 text-white p-2 my-3" onClick={()=>handleEnableMultiSelect()}>Enable Multi Select</button>
+                    </div>
+                    {
+                        data.map(item=>(
+
+                            <div key={item.id} className="bg-blue-700 text-white py-3 mb-2  px-4 rounded-md cursor-pointer">
+
+                                <div className="text-center  flex justify-between items-center" onClick={enableMultiSelect ? ()=>handleMultiSelect(item.id) : ()=>handleSingleSelect(item.id)}>
+                                    <div className="font-bold ">
+                                        {item.question}
+                                    </div>
+                                    <div>
+                                            +
+                                    </div>
+                                </div>
+                                {
+                                   multipleSelected.indexOf(item.id) !== -1 || selected === item.id ? (
+                                        <div className="text-center mt-3">
+                                            {item.answer}
+                                        </div>
+                                    ) : null
+                                }
+                            </div>
+
+                        ))
+                    }
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Index
